a1b8100f9a16fc58d689b6a2873c7cd1a7b1ee73
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-17 01:53:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,16 @@
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
+  if  phrase == "I LOVE YOU GRANDMA!"
+    return  "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    return  "HUH?! SPEAK UP, SONNY!"
+  else  phrase == "WHAT DID YOU EAT TODAY?"
+    return  "NO, NOT SINCE 1938!"
+  end
+end
+
+speak_to_grandma("I LOVE YOU GRANDMA!")
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
